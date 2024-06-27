dc850c365dc2f6f07013bbb56f184b37d7bed778
BonJere <jerebon2552@gmail.com>
2024-06-27 13:32:50 -0300

    trabajo hecho

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    const nav = document.getElementById('nav');
+    nav.classList.toggle('active');
+}
+
+function toggleTheme() {
+    document.body.classList.toggle('dark-mode');
+}
