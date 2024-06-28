53b7ff64637249a58e8789347d471c8c26a45a39
Nacho30 <ignaciomartinrodriguez23@gmail.com>
2024-06-28 17:36:05 -0300

    pagina terminada

--- script.js
+++ script.js
@@ -6,3 +6,16 @@ function toggleMenu() {
 function toggleTheme() {
     document.body.classList.toggle('dark-mode');
 }
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuLinks = document.querySelectorAll('nav ul li a');
+    const nav = document.querySelector('nav');
+
+    menuLinks.forEach(link => {
+        link.addEventListener('click', function() {
+            if (nav.classList.contains('active')) {
+                nav.classList.remove('active');
+            }
+        });
+    });
+});
